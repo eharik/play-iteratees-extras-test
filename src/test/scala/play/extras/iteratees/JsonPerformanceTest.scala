4f4a7a0a5a16dd4a68e8f7d019f6c9f0dd8c5fde
Elliot P. Harik <elliot@oseberg.io>
2016-01-19 17:41:53 -0500

    perf testing: demonstrate non linear performance

--- src/test/scala/play/extras/iteratees/JsonPerformanceTest.scala
+++ src/test/scala/play/extras/iteratees/JsonPerformanceTest.scala
@@ -1,27 +1,38 @@
 package play.extras.iteratees
 
-import scala.io.Source
+import java.io.File
 import play.api.libs.iteratee._
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 
 object JsonPerformanceTest extends App {
 
-  val events = Source.fromInputStream(JsonPerformanceTest.getClass.getResourceAsStream("/events.json"))
-    .mkString.toCharArray.grouped(8192).toSeq.map(CharString.fromCharArray)
-
-  def test = {
-    Await.result(Enumerator.enumerate(events) |>>> JsonIteratees.jsSimpleArray, Duration.Inf)
+  def jsonStream(fn: String) = {
+    Enumerator.fromFile(new File(s"./src/test/resources/$fn")) &>
+      Encoding.decode() &>
+      JsonEnumeratees.jsArray
   }
+
+  def test(fileName: String = "events.json") = {
+    Await.result(
+      jsonStream(fileName) |>>> Iteratee.ignore,
+      Duration.Inf)
+  }
+
   def time(block : => Unit): Long = {
     val start = System.currentTimeMillis()
     block
     System.currentTimeMillis() - start
   }
 
-  println("Warmup: " + time((1 to 10).foreach(_ => test)) + "ms")
-  println("Run 1: " + time((1 to 10).foreach(_ => test)) + "ms")
-  println("Run 2: " + time((1 to 10).foreach(_ => test)) + "ms")
+  println("<----- Degraded Performance Test ---->")
+  println("Warmup: " + time((1 to 10).foreach(_ => test())) + "ms")
+  println("1x:   " + time(test("events.json")) + "ms")
+  println("10x:  " + time(test("events10x.json")) + "ms")
+  println("100x: " + time(test("events100x.json")) + "ms")
+  println("200x: " + time(test("events200x.json")) + "ms")
+  println("400x: " + time(test("events400x.json")) + "ms")
+  println("800x: " + time(test("events800x.json")) + "ms")
 
 }
